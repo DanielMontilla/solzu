a1db642795e73fa02cd31240aa0ea59933054dfb
Daniel Montilla <montilla.d2000@gmail.com>
2023-09-25 20:29:37 -0400

    `package.json` path fix

--- scripts/check_version.ts
+++ scripts/check_version.ts
@@ -1,7 +1,7 @@
 const decoder = new TextDecoder("utf-8");
 
 // Read the package.json file
-const packageContent = await Deno.readFile("../package.json");
+const packageContent = await Deno.readFile("./package.json");
 const packageJson = JSON.parse(decoder.decode(packageContent));
 const currentVersion = packageJson.version;
 

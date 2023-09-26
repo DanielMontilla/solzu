9430b48542f677d66456430a6bf3fe43c891594d
Daniel Montilla <montilla.d2000@gmail.com>
2023-09-25 20:22:17 -0400

    deno setup for `/scripts` & `check_version` script

---
+++ scripts/check_version.ts
@@ -0,0 +1,18 @@
+const decoder = new TextDecoder("utf-8");
+
+// Read the package.json file
+const packageContent = await Deno.readFile("../package.json");
+const packageJson = JSON.parse(decoder.decode(packageContent));
+const currentVersion = packageJson.version;
+
+// Fetch the version from the npm registry
+const response = await fetch(`https://registry.npmjs.org/${packageJson.name}`);
+const data = await response.json();
+const npmVersion = data["dist-tags"].latest;
+
+if (currentVersion <= npmVersion) {
+  console.error(`Current version (${currentVersion}) is not greater than the npm version (${npmVersion}).`);
+  Deno.exit(1);
+} else {
+  console.log(`Current version (${currentVersion}) is greater than the npm version (${npmVersion}). Ready to publish!`);
+}

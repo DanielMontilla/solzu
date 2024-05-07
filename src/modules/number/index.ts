86347348151e122693c12af29038a5bf6e7d002f
Daniel Montilla <montilla.d2000@gmail.com>
2024-05-07 15:29:10 -0400

    added `number` module

--- src/modules/number/index.ts
+++ src/modules/number/index.ts
@@ -6,3 +6,12 @@
 export function isNumber(x: unknown): x is number {
   return typeof x === "number" && !Number.isNaN(x);
 }
+
+/**
+ * Checks if the value is an integer
+ * @param {unknown} x value to check
+ * @returns {boolean} `true` if x is an integer, `false` otherwise.
+ */
+export function isInt(x: unknown): x is number {
+  return isNumber(x) && Number.isInteger(x);
+}

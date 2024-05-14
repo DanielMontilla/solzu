4d45ed8cbe4e74a03b813142fc52991642f24e6b
Daniel Montilla <montilla.d2000@gmail.com>
2024-05-14 13:08:36 -0400

    improved typings for `defineEnum`

--- src/modules/macros/index.ts
+++ src/modules/macros/index.ts
@@ -1,13 +1,12 @@
 /**
  * Creates a readonly object representing an enumeration from the provided keys.
- * Each key is mapped to a value that is exactly the same as the key.
- * The provided array must be annotated with `as const` for the macro to correctly type the enum.
+ * Each key is mapped to a value that is exactly the same as the key
  *
  * @template Keys A tuple type of string literals representing the keys of the enumeration.
  * @param {Keys} keys An array of strings representing the keys of the enumeration.
  * @returns {Readonly<{ [K in Keys[number]]: K }>} A readonly object with keys and values equal to the elements of the input array.
  */
-export function defineEnum<Keys extends readonly string[]>(
+export function defineEnum<const Keys extends readonly string[]>(
   keys: Keys
 ): Readonly<{ [K in Keys[number]]: K }> {
   return keys.reduce((obj, key) => {

1781852cda424789a1de2a0cc4e6ba8b6fbf3bd5
Daniel Montilla <montilla.d2000@gmail.com>
2024-06-07 01:24:01 -0400

    configuring `tsup`

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,3 @@
-import { resolve } from "path";
 import { defineConfig } from "vitest/config";
-import dts from "vite-plugin-dts";
 
-export default defineConfig({
-  build: {
-    lib: {
-      entry: {
-        index: resolve(__dirname, "src/index.ts"),
-        "primitives/result/scoped": resolve(
-          __dirname,
-          "src/primitives/result/scoped.ts"
-        ),
-        "primitives/maybe/scoped": resolve(
-          __dirname,
-          "src/primitives/maybe/scoped.ts"
-        ),
-      },
-      name: "solzu",
-      formats: ["es", "cjs"],
-      fileName: (format, name) =>
-        format === "es" ? `${name}.js` : `${name}.${format}`,
-    },
-    minify: true,
-  },
-  plugins: [dts()],
-});
+export default defineConfig({});

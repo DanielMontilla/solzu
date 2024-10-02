e18e04d0d872707d7e7491b11b8fee33e7ccf31c
Daniel Montilla <montilla.d2000@gmail.com>
2024-10-02 00:09:10 -0400

    added new exports `string`, `reactive` and `random`

--- packages/solzu/src/index.ts
+++ packages/solzu/src/index.ts
@@ -2,10 +2,14 @@ export * from "./types";
 
 /** Primitives Extensions */
 export * from "./number";
+export * from "./string";
 export * from "./object";
 export * from "./array";
 export * from "./function";
 export * from "./promise";
+
+export * from "./reactive";
+export * from "./random";
 
 /** Data structures */
 export * from "./nothing";
